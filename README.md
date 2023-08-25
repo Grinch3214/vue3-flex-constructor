d71f13ab52da1452b2bf2d788059723979c57788
Max <scorpiomax90@gmail.com>
2023-08-25 10:41:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Flex-constructor
+a live project with [vueJs](http://vuejs.org)
+
+<!-- ## Demo
+ Go to https://grinch3214.github.io/vue3-nasa-project/ -->
+
+ <!-- ## Preview
+
+<img src="https://github.com/Grinch3214/vue3-nasa-project/blob/main/demo/Screenshot_2.png" width=500 />
+
+<img src="https://github.com/Grinch3214/vue3-nasa-project/blob/main/demo/demoGif.gif" /> -->
+
+---
+
+ ## Technologies used
+
+- **Vue.js** (Vue 3)
+- **CSS** (scss)
+- **Vite**
+
+---
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run dev
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+		
+	</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+@font-face {
+	font-family: 'Open Sans';
+	src: url('./assets/fonts/OpenSans-Regular.woff2') format('woff2');
+	font-weight: 400;
+	font-style: normal;
+	font-display: swap;
+}
+
+@font-face {
+	font-family: 'Open Sans';
+	src: url('./assets/fonts/OpenSans-SemiBold.woff2') format('woff2');
+	font-weight: 600;
+	font-style: normal;
+	font-display: swap;
+}
+
+@font-face {
+	font-family: 'Open Sans';
+	src: url('./assets/fonts/OpenSans-ExtraBold.woff2') format('woff2');
+	font-weight: 800;
+	font-style: normal;
+	font-display: swap;
+}
+</style>
+
